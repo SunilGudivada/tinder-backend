fcc7f7ca9ba424cdc92af897f443376c8002eba0
Sunil Gudivada <sunil.gudivada@willhire.co>
2021-09-11 17:37:20 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cors from 'cors';
+import Cards from "./dbCards.js";
+
+//szu87g0MIyhoHGZS
+
+
+// App config
+const app = express();
+const port = process.env.PORT || 8001
+const connection_url = "mongodb+srv://admin:szu87g0MIyhoHGZS@cluster0.chhxu.mongodb.net/tinderdb?retryWrites=true&w=majority"
+
+// Middlewares
+app.use(express.json())
+app.use(Cors());
+
+// DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+// API Endpoints
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello World")
+})
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err,data) => {
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+    
+})
+
+app.get('/tinder/cards', (req,res) => {
+    Cards.find((err,data) => {
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
+
+// Listener
+app.listen(port, () => console.log(`Listening on localhost:: ${port}`));
